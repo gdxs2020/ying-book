d14122d75fc6fef85be4356882e32c59783ae596
gdxs2018 <gdxs2018@126.com>
2020-02-24 20:06:09 +0800

    2020

---
+++ book-service/src/main/java/com/cc/yingBook/bookInfo.java
@@ -0,0 +1,36 @@
+package com.cc.yingBook;
+
+/**
+ * Hello world!
+ *
+ */
+public class bookInfo
+{
+    private String bookName;
+    private Integer bookId;
+    private Integer bookPrice;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public Integer getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(Integer bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+}
